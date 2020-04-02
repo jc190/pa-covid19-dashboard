757cdbda5741de4a8a7261032ac2e1a80f9b9f79
James Calhoun <jim.calhoun@outlook.com>
2020-04-02 15:05:18 -0400

    build

---
+++ build/precache-manifest.d2ea11ece06e89bd94b9465ebaa9482f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151b740770559dd5a11d3addbe7903b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36a1f47a21e6dc738469",
+    "url": "/static/css/2.3cf71c33.chunk.css"
+  },
+  {
+    "revision": "fafc15cc80654ec1dd2d",
+    "url": "/static/css/main.36172b07.chunk.css"
+  },
+  {
+    "revision": "36a1f47a21e6dc738469",
+    "url": "/static/js/2.229d0b28.chunk.js"
+  },
+  {
+    "revision": "19d24ae19ee06c3a97936261381c1fb8",
+    "url": "/static/js/2.229d0b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafc15cc80654ec1dd2d",
+    "url": "/static/js/main.754b9ffa.chunk.js"
+  },
+  {
+    "revision": "e8f59f5907206e1051cd",
+    "url": "/static/js/runtime-main.7f2a6d6f.js"
+  }
+]);
