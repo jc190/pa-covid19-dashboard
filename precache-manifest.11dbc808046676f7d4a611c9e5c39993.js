52f781d6a577aec2f1f4e9e979f2663d70acaa8c
James Calhoun <jim.calhoun@outlook.com>
2020-04-02 15:15:28 -0400

    Updates

---
+++ precache-manifest.11dbc808046676f7d4a611c9e5c39993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e312ba4a10759efd18ac4bbb043dfe98",
+    "url": "/pa-covid19-dashboard/index.html"
+  },
+  {
+    "revision": "36a1f47a21e6dc738469",
+    "url": "/pa-covid19-dashboard/static/css/2.3cf71c33.chunk.css"
+  },
+  {
+    "revision": "59c8def5f497c617a721",
+    "url": "/pa-covid19-dashboard/static/css/main.36172b07.chunk.css"
+  },
+  {
+    "revision": "36a1f47a21e6dc738469",
+    "url": "/pa-covid19-dashboard/static/js/2.229d0b28.chunk.js"
+  },
+  {
+    "revision": "19d24ae19ee06c3a97936261381c1fb8",
+    "url": "/pa-covid19-dashboard/static/js/2.229d0b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c8def5f497c617a721",
+    "url": "/pa-covid19-dashboard/static/js/main.dd4e7e73.chunk.js"
+  },
+  {
+    "revision": "309b36f51f15b90a8758",
+    "url": "/pa-covid19-dashboard/static/js/runtime-main.c49a85fd.js"
+  }
+]);
